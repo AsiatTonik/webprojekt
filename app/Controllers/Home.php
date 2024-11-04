c6145412075c79e6e8cd17804eabcec59f52a520
davidvyc <91115833+davidvyc@users.noreply.github.com>
2024-11-04 09:53:22 +0100

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,39 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+    public function index(Request $request)
+    {
+        $items = Item::paginate(10); // Stránkování
+        return view('items.index', compact('items'));
+    }
+
+    public function create()
+    {
+        return view('items.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Validace a uložení položky
+        Item::create($request->all());
+        return redirect()->route('items.index');
+    }
+
+    public function edit(Item $item)
+    {
+        return view('items.edit', compact('item'));
+    }
+
+    public function update(Request $request, Item $item)
+    {
+        // Validace a aktualizace položky
+        $item->update($request->all());
+        return redirect()->route('items.index');
+    }
+
+    public function destroy(Item $item)
+    {
+        $item->delete();
+        return redirect()->route('items.index');
+    }
 }
